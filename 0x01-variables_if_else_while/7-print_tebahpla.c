c1b7bfba9d777c96f5615a21fc4b310aeac1e7a2
Solomon Aggrey <saggrey96@yahoo.com>
2023-09-12 06:11:58 +0000

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Print lowercase in revers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char m;
+
+	for (m = 'z'; m >= 'a'; m--)
+
+	{
+		putchar(m);
+	}
+	putchar('\n');
+	return (0);
+}
