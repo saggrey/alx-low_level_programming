4791377ca3b60b91ddf102119912002b2a3c158e
Solomon Aggrey <saggrey96@yahoo.com>
2023-09-12 05:46:34 +0000

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Aprogram that prints single digit numbers of base 10 from 0
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+		for (n = 48; n < 58; n++)
+		{
+			putchar(n);
+
+		}
+	putchar('\n');
+	return (0);
+}
