1cb6be645318daffc08f0311f190c4cca16d731e
Solomon Aggrey <saggrey96@yahoo.com>
2023-09-12 07:50:09 +0000

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints  single-digit numbers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 48; a <= 57; a++)
+	{
+		putchar(a);
+		if (a != 57)
+		{
+			putchar(',');
+			putchar(' ');
+	}
+	}
+	putchar('\n');
+	return (0);
+}
