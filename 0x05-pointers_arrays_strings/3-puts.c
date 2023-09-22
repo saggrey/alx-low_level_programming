1cffbadff7e2820ce139a9951b8aa2844bf75c45
Solomon Aggrey <saggrey96@yahoo.com>
2023-09-22 23:59:21 +0000

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *  _puts - prints a string followed by a new line..
+ *
+ * @str: the string to print.
+ *
+ */
+
+void _puts(char *str)
+{
+	int x;
+
+	for (x = 0; str[i] != '\0'; x++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
