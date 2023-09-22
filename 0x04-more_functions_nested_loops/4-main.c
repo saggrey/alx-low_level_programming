e7abdc32010f713e08c7cd81a33c1548067a2ea5
Solomon Aggrey <saggrey96@yahoo.com>
2023-09-22 00:55:50 +0000

    4-print_most_numbers

---
+++ 0x04-more_functions_nested_loops/4-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - check the code for alx serious students.
+ *
+ * Return: Always 0.
+ */
+void print_most_numbers(void);
+int main(void)
+{
+	print_most_numbers();
+	return (0);
+}
